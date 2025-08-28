2a7426c6e0fa02b44727f405ab0b75b46c3adfb0
Sid-Purk <sidpurk04@gmail.com>
2025-08-29 00:40:32 +0530

    Added Model Dev Logic & Huggingface API

--- backend/app/api/similarity_search.py
+++ backend/app/api/similarity_search.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Request
 from pinecone import Pinecone
 from app.config import PINECONE_API_KEY,PINECONE_INDEX
-from app.services.model_service import get_model,model
+# from app.services.model_service import get_model,model
+from huggingface_hub import InferenceClient
 
 router=APIRouter()
 
 pc=Pinecone(api_key=PINECONE_API_KEY)
 index=pc.Index(PINECONE_INDEX)
+
+client=InferenceClient("Sid-the-sloth/leetcode_unixcoder_final")
 
 @router.post('/api/similar_search')
 async def similar_search(request: Request):
@@ -16,7 +19,8 @@ async def similar_search(request: Request):
     combined=f"Name: {metadata['title']} | Code: {metadata['code']} | Tags: {','.join(metadata['tags'])}"
 
     # model=get_model()
-    embedding=model.encode(combined)
+    # embedding=model.encode(combined)
+    embedding=client.feature_extraction(combined)
 
     res=index.query(vector=embedding.tolist(),top_k=50, include_metadata=True)['matches']
     for meta in res:
