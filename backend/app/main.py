6c92dda5f8faa54f0288b17bcdb7fc851f75a408
Sid-Purk <sidpurk04@gmail.com>
2025-08-29 01:06:26 +0530

    Api Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,17 +3,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
-from app.services.model_service import get_model
-from contextlib import asynccontextmanager
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    get_model()
-    yield
+# from app.services.model_service import get_model
+# from contextlib import asynccontextmanager
 
 
 limiter=Limiter(key_func=get_remote_address)
-app=FastAPI(lifespan=lifespan)
+app=FastAPI()
 app.state.limiter=limiter
 app.add_exception_handler(429, _rate_limit_exceeded_handler)
 
